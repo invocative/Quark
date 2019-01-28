7ab74bb18cb69b8ec68ad6a5571f1e2db115501b
Yuuki Wesp <ls-micro@ya.ru>
2019-01-28 08:06:30 +0300

    Create QuarkType.cs

---
+++ QuarkType.cs
@@ -0,0 +1,33 @@
+namespace Quark
+{
+    public enum QuarkType
+    {
+        /// <summary>
+        /// D-Quark (Down-Quark)
+        /// </summary>
+        d,
+        /// <summary>
+        /// U-Quark (Up-Quark)
+        /// </summary>
+        u,
+        /// <summary>
+        /// S-Quark (strange quark)
+        /// </summary>
+        s,
+        /// <summary>
+        /// C-Quark (charmed quark)
+        /// </summary>
+        c,
+        /// <summary>
+        /// B-Quark (bottom quark)
+        /// </summary>
+        b,
+        /// <summary>
+        /// T-Quark (top quark) 
+        /// </summary>
+        /// <remarks>
+        /// LIKE A BOSS))00)!1!!1!
+        /// </remarks>
+        t
+    }
+}
