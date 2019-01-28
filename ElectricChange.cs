98f0fa477791d0504850d72d4aed4560d5afa562
Yuuki Wesp <ls-micro@ya.ru>
2019-01-28 08:39:24 +0300

    Create ElectricChange.cs

---
+++ ElectricChange.cs
@@ -0,0 +1,32 @@
+namepspace Quark
+{
+    using Sprache;
+    using System;
+    
+    public class ElectricChange
+    {
+        public char Unit = '\u212F';
+        public bool IsPositive;
+        public string Value;
+
+
+
+        public ElectricChange(bool Positive, string value)
+        {
+            IsPositive = Positive;
+            Value = value;
+        }
+
+        public static Parser<ElectricChange> Token = 
+            from first in Parse.Char('+').Or(Parse.Char('-'))
+            from _1 in Parse.Char('(')
+            from n1 in Parse.Number
+            from del in Parse.Char('/')
+            from n2 in Parse.Number
+            from _2 in Parse.Char(')')
+            select new ElectricChange(first.Equals('+'), $"{n1}/{n2}");
+
+
+        public override string ToString() => $"{(IsPositive ? "+" : "-")}({Value}){Unit}";
+    }
+}
