ee524d114daaab6e9f6618161a4ff539c0bc7d6e
Yuuki Wesp <ls-micro@ya.ru>
2019-01-28 10:47:23 +0300

    add Quark

---
+++ lib/Quark.cs
@@ -0,0 +1,32 @@
+﻿namespace QuarkParser
+{
+    using Sprache;
+
+    public class Quark
+    {
+        /// <summary>
+        /// Quark Name
+        /// </summary>
+        public string Name { get; protected set; }
+        /// <summary>
+        /// Quark Symbol
+        /// </summary>
+        public char Symbol { get; protected set; }
+        /// <summary>
+        /// Quark Type
+        /// </summary>
+        public QuarkType Type { get; protected set; }
+        /// <summary>
+        /// Electric Change
+        /// </summary>
+        public ElectricChange EChange { get; protected set; }
+        /// <summary>
+        /// Quark Mass
+        /// </summary>
+        public float Mass { get; protected set; }
+
+
+
+        //public static Parser<Quark>
+    }
+}
