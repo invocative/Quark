51369831a227125b92678e6f4d5f5b5551a91cf0
Yuuki Wesp <ls-micro@ya.ru>
2019-07-04 06:47:04 +0300

    add optional parse special symbols of energy

--- lib/ElectricChange.cs
+++ lib/ElectricChange.cs
@@ -4,38 +4,33 @@ namespace Elementary.Quarks
 
     public class ElectricChange
     {
-        public char Unit = '\u212F';
-        public bool IsPositive;
-        public string Value;
+        public static char Unit = '\u212F';
+        public bool IsPositive { get; set; }
+        public string Value { get; set; }
 
 
 
-        public ElectricChange(bool Positive, string value)
+        public ElectricChange(bool positive, string value)
         {
-            IsPositive = Positive;
+            IsPositive = positive;
             Value = value;
         }
 
         public static Parser<ElectricChange> Token =
-            from first in Parse.Char('+').Or(Parse.Char('-'))
-            from _1 in Parse.Char('(')
-            from n1 in Parse.Number
-            from del in Parse.Char('/')
+            from first in Parse.Char('+').Or(Parse.Char('-')) // plus '+' or minus '-'
+            from _1 in Parse.Char('(')  // open brace
+            from n1 in Parse.Number         
+            from del in Parse.Char('/') // divider
             from n2 in Parse.Number
-            from _2 in Parse.Char(')')
+            from _2 in Parse.Char(')')  // closed brace
+            from unit in Parse.Char(Unit).Optional()
             select new ElectricChange(first.Equals('+'), $"{n1}/{n2}");
 
 
         public override string ToString() => $"{(IsPositive ? "+" : "-")}({Value}){Unit}";
 
 
-        public static implicit operator string(ElectricChange e)
-        {
-            return e.ToString();
-        }
-        public static implicit operator ElectricChange(string e)
-        {
-            return Token.Parse(e);
-        }
+        public static implicit operator string(ElectricChange e) => e.ToString();
+        public static implicit operator ElectricChange(string e) => Token.Parse(e);
     }
 }
