ed738b56662ceacfd86141e47ccf2ee82b8bafed
Yuuki Wesp <ls-micro@ya.ru>
2019-07-04 06:47:23 +0300

    add test

--- test/ElectricChange.cs
+++ test/ElectricChange.cs
@@ -9,6 +9,7 @@ namespace unit_test
         {
             Assert.Equal("+(1/2)\u212F", $"{(ElectricChange)"+(1/2)"}");
             Assert.Equal("-(4/7)\u212F", $"{(ElectricChange)"-(4/7)"}");
+            Assert.Equal("-(4/7)\u212F", $"{(ElectricChange)"-(4/7)\u212F"}");
         }
     }
 }
