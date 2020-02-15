8ef9626d91280c98ab6baeb201686e13615e5e75
Yuuki Wesp <ivysola@0xf6.moe>
2020-02-15 16:28:53 +0300

    add flavor strengths index\correlation

--- lib/Quark.cs
+++ lib/Quark.cs
@@ -2,9 +2,11 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using eV.Measure;
     using Quarks;
     using Sprache;
+    using static QuarkType;
 
     public class Quark
     {
@@ -12,7 +14,7 @@
         {
             this.Mass = new Energy(0.0, Energy.ElectronVolt);
             this.Name = "Unknown";
-            this.Type = QuarkType.Unk;
+            this.Type = Unk;
             this.EChange = "-(0/0)";
             this.Prefix = (!isAnti ? '+' : '-');
         }
@@ -31,6 +33,8 @@
         
         protected string InternalChar { get; set; }
         protected string InternalAntiChar { get; set; }
+
+        protected QuarkWeakType weakType { get; set; }
 
         /// <summary>
         /// Quark Symbol
@@ -58,6 +62,40 @@
         /// </summary>
         public Energy Mass { get; protected set; }
 
+        /// <summary>
+        /// While the process of flavor transformation is the same for all quarks,
+        /// each quark has a preference to transform into the quark of its own generation.
+        /// </summary>
+        /// <returns>
+        /// approximate magnitudes
+        /// </returns>
+        public float GetFlavorTransformationIndexAt(Quark quark)
+        {
+            ((QuarkType, QuarkType), float?)[,] flavor = {
+                { ((u, d), 0.974f), ((u, s), 0.225f),  ((u, b), 0.003f)  },
+                { ((c, d), 0.225f), ((c, s), 0.973f),  ((c, b), 0.041f)  },
+                { ((t, d), 0.009f), ((t, s), 0.040f),  ((t, b), 0.999f)  },
+            };
+            return GetDecayDataByPair(quark, this, flavor);
+        }
+        /// <summary>
+        /// While the process of flavor transformation is the same for all quarks,
+        /// each quark has a preference to transform into the quark of its own generation.
+        /// </summary>
+        /// <returns>
+        /// approximate magnitudes correlation
+        /// </returns>
+        public float GetStrengthsСorrelationAt(Quark quark)
+        {
+            ((QuarkType, QuarkType), float?)[,] сorrelation = {
+                { ((u, d), 1.00f), ((u, s), 0.50f),  ((u, b), 0.25f)  },
+                { ((c, d), 0.50f), ((c, s), 1.00f),  ((c, b), 0.25f)  },
+                { ((t, d), 0.25f), ((t, s), 0.25f),  ((t, b), 1.00f)  },
+            };
+            return GetDecayDataByPair(quark, this, сorrelation);
+        }
+
+
         public override string ToString() => $"{Symbol} {EChange} {Mass[Energy.MegaElectronVolt]}";
 
 
@@ -80,9 +118,25 @@
             select new List<Quark>(q);
 
 
-        private static readonly Parser<Quark> privateToken =
+        internal static readonly Parser<Quark> privateToken =
             from dig in Parse.Chars("+-").Optional()
             from sym in Parse.Chars("duscbt")
             select QuarkBySymbol(sym, dig.GetOrDefault() == '-');
+
+        internal static float GetDecayDataByPair(Quark f1, Quark f2, ((QuarkType, QuarkType) pair, float? index)[,] data)
+        {
+            static IEnumerable<T> Flatten<T>(T[,] matrix) {
+                foreach (var item in matrix) yield return item;
+            }
+
+            float? select(Func<((QuarkType, QuarkType) pair, float? index), bool> selector) =>
+                Flatten(data).FirstOrDefault(selector).index;
+
+            var result =
+                select(z => z.pair == (f1.Type, f2.Type)) ??
+                select(z => z.pair == (f2.Type, f1.Type));
+
+            return result ?? 0.0f;
+        }
     }
 }
