4f0399280f2e4f781d186c37e3b6bd27dc9128c0
Yuuki Wesp <ivysola@0xf6.moe>
2020-02-18 18:43:35 +0300

    add constants

---
+++ lib/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace Elementary.Quarks
+{
+    internal static class Constants
+    {
+        public const double ElectronMassPlank = 1.6000000000000002E-19;
+        public const double SpeedOfLightSquared = 9E+16;
+    }
+}
