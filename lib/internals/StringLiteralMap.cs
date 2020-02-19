61b737fe80423e89df34e7753ec54b75807ff82c
Yuuki Wesp <ivysola@0xf6.moe>
2020-02-19 20:47:10 +0300

    add literal string storage

---
+++ lib/internals/StringLiteralMap.cs
@@ -0,0 +1,23 @@
+﻿namespace Elementary.Primitives.@internal
+{
+    using System.Collections.Generic;
+
+    internal static class StringLiteralMap
+    {
+        private static readonly Dictionary<QuarkType, string> names;
+        static StringLiteralMap()
+        {
+            names = new Dictionary<QuarkType, string>
+            {
+                {QuarkType.c, "Charm Quark"}, 
+                {QuarkType.s, "Strange Quark"},
+                {QuarkType.d, "Down Quark"},
+                {QuarkType.u, "Up Quark"},
+                {QuarkType.b, "Beauty Quark"},
+                {QuarkType.t, "Top Quark"}
+            };
+        }
+
+        internal static string GetNameByType(QuarkType type) => names[type];
+    }
+}
